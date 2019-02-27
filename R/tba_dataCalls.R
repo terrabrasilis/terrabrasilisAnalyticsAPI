5517e9a397f16a09bcc4fd0e07f4388b6a1be352
Luiz Fernando Ferreira Gomes de Assis <luizffga@dpi.inpe.br>
2019-02-27 08:51:22 -0300

    include get data by loiname and by parameters

---
+++ R/tba_dataCalls.R
@@ -0,0 +1,45 @@
+#' list data by loiname
+#'
+#' @param apiPath path of terrabrasilis analytics server API
+#' @param appIdentifier define the application identifier
+#' @param class define the application identifier
+#' @param loiname define the application identifier
+#' 
+#' @name tba_get_dataByLoiname
+#' @export
+tba_get_dataByLoiname <- function(apiPath, appIdentifier, class, loiname) {
+  
+  # define config header
+  h <- tba_dataHeader(curl::new_handle(), appIdentifier)
+  
+  # request locals of interests with a specific header
+  resJSON <- tba_request(paste(apiPath, "data/", class, "/", loiname, sep = ""), h)
+  
+  # wrangle data by loiname as tibble
+  return(resJSON)
+  
+}
+
+#' list all lois
+#'
+#' @param apiPath a curl handle used to configure a request
+#' @param appIdentifier define the application identifier
+#' 
+#' @name tba_get_dataByParameters
+#' @export
+tba_get_dataByParameters <- function(apiPath, appIdentifier, class, loiname, startDate, endDate) {
+  
+  # define config header
+  h <- tba_configHeader(curl::new_handle(), appIdentifier)
+  
+  # request locals of interests with a specific header
+  resJSON <- tba_request(paste(apiPath, 
+                               "data/query?class=", class, 
+                               "&loiname", loiname, 
+                               "&startDate", startDate, 
+                               "&endDate", endDate, sep = ""), h)
+  
+  # wrangle data by parameters respose into tibble
+  return(tibble::as_tibble(resJSON))
+  
+}
