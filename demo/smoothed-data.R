e36415c2dd67354b46811923d2c5418f7cc078d2
Luiz Fernando Assis <luizffga@gmail.com>
2019-07-18 19:03:17 -0300

    alter package description for demo

--- demo/smoothed-data.R
+++ demo/smoothed-data.R
@@ -3,20 +3,20 @@ require(lubridate)
 require(ggplot2)
 
 # define path for Terrabrasilis Analytics API
-tbaAPIPath <- "http://terrabrasilis.dpi.inpe.br/dashboard/api/v1/redis-cli/"
+apiPath <- "http://terrabrasilis.dpi.inpe.br/dashboard/api/v1/redis-cli/"
 
 # get all the application identifier
-appIdentifier <- tba_list_apps_identifier(tbaAPIPath)
+appIdentifier <- list_datasets(apiPath)
 
 # define PRODES Cerrado variable
 prodesCerrado <- appIdentifier$identifier[1]
 
 # get all loinames from states (UF)
 loiUF = dplyr::filter(lois, grepl("UF", name))$gid
-loinamesByLoi <- tba_list_loinamesByLoi(tbaAPIPath, appIdentifier$identifier[1], loiUF)
+loinamesByLoi <- list_localOfInterestsByLocal(apiPath, appIdentifier$identifier[1], loiUF)
 
 # get all data by loiname
-data <- tba_get_dataByLoiname(tbaAPIPath, prodesCerrado, classes$name, "all")
+data <- get_dataByLoiname(tbaAPIPath, prodesCerrado, classes$name, "all")
 
 # assign start date
 startDate <- data$periods$startDate
